2e4e3dcd3de3e2caa536b2a4fb7227e8e1a5235d
pythonier <vicjohn2018@gmail.com>
2020-01-06 00:56:35 +0100

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -4,7 +4,7 @@ a = 5
 b = 6
 c = 7
 
-Uncomment below to take inputs from the user
+
 a = float(input('Enter first side: '))
 b = float(input('Enter second side: '))
 c = float(input('Enter third side: '))
